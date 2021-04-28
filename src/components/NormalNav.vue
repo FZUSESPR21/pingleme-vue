910d85502bd8ebcb95e60457c35e1d6be9a04a0d
jdEarm <1843046797@qq.com>
2021-04-28 20:55:08 +0800

    CreateEvaluation part

---
+++ src/components/NormalNav.vue
@@ -0,0 +1,48 @@
+<template>
+  <div style="width: 200px" id="navleft">
+    <a-menu
+      :default-selected-keys="['4']"
+      :default-open-keys="['sub1']"
+      mode="inline"
+      theme="light"
+      :inline-collapsed="collapsed"
+    >
+      <a-menu-item key="1">
+        <a-icon type="user" />
+        <span>个人信息</span>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <a-icon type="book" />
+        <span>作业列表</span>
+      </a-menu-item>
+      <a-menu-item key="3">
+        <a-icon type="file-done" />
+        <span>成绩列表</span>
+      </a-menu-item>
+	  <a-menu-item key="4">
+	    <a-icon type="form" />
+	    <span>创建评审表</span>
+	  </a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script>
+export default {
+	name:'NormalNav',
+	data() {
+		return {
+		collapsed: false,
+		};
+	},
+	methods: {
+		toggleCollapsed() {
+		this.collapsed = !this.collapsed;
+    },
+  },
+};
+</script>
+
+<style>
+	
+</style>
---
+++ src/normal/CreateEvaluationForm.vue
@@ -0,0 +1,59 @@
+<template>
+	<a-layout id="components-layout-demo-evaluationform-trigger">
+		<a-layout-sider v-model="collapsed" :trigger="null" collapsible id="formside1">
+		    <div class="formlogo">
+				<a-button type="link"
+						style="margin-bottom: 16px; margin-top: 10px;"
+						@click="() => (collapsed = !collapsed)">
+				  <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+							&nbsp;PingLeMe
+				</a-button>
+			</div>
+		    <NormalNav></NormalNav>
+		</a-layout-sider>
+		
+		<a-layout>
+			<a-layout-header style="background: #fff;font-size: medium;">
+				<div class="evainfo">
+					<a-icon type="user" />&nbsp;用户：XXX
+					<a-divider type="vertical" />
+					<a-icon type="team" />&nbsp;团队：XXX团队
+					<a-divider type="vertical" />
+					<a-icon type="team" />&nbsp;作业：作业XXX
+				</div>
+			</a-layout-header>
+			<hr>
+			<a-layout-content :style="{ margin: '0px 0px', padding: '16px', background: '#fff', minHeight: '280px' }">
+				<EvaCrForm></EvaCrForm>
+			</a-layout-content>	
+		</a-layout>
+	</a-layout>
+</template>
+
+<script>
+	import NormalNav from "../components/NormalNav.vue";
+	import EvaCrForm from "../components/EvaCrForm.vue";
+	export default{
+		name:'CrEvaform',
+		components:{
+			EvaCrForm,
+			NormalNav,
+		},
+		data(){
+			return{
+				collapsed:false,
+			}
+		}
+	}
+</script>
+
+<style>
+#components-layout-demo-evaluationform-trigger .formlogo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0);
+  margin: 16px;
+}
+#formside1{
+	background-color: white;
+}
+</style>
