a456b34a3a58ce2408bb17c8969fea0ba6dd1be0
Tin <48679438+smsbQAQ@users.noreply.github.com>
2021-05-12 10:10:27 +0800

    limit button login

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ import "ant-design-vue/dist/antd.css";
 import router from "./router"
 import store from "./store"
 import { FormModel } from 'ant-design-vue';
+<<<<<<< Updated upstream
+=======
+import echarts from "echarts"
+import ElementUI from "element-ui"
+import './mock/mock'
+import axios from 'axios'
+import '../config/axios'
+>>>>>>> Stashed changes
 
 
 Vue.use(FormModel);
 Vue.use(Antd);
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
